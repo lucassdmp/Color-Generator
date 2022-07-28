99f3f917b6f3b97fffe0fb414a472a4405242e43
lucassdmp <butwhylucas@gmail.com>
2022-07-28 15:31:15 -0300

    Gerador de Cores

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const colors = ["red", "green", "blue", "black", "white", "pink", "yellow", "orange", "gray"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+
+btn.addEventListener('click', function(){
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
